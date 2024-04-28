25b37bbe67e69d8a7dba6c5cdd864fa0a9b6f73f
Paul Ockenfuss <42680748+Ockenfuss@users.noreply.github.com>
2024-04-29 00:22:12 +0200

    Grand Jatte Example

---
+++ examples/grande_jatte.py
@@ -0,0 +1,16 @@
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+from crazybin import imshow
+
+imagepath='images/grande_jatte_seurat.jpg'
+image=plt.imread(imagepath)/255
+#%%
+fig, ax=plt.subplots(figsize=(7,7))
+imshow(image, tile='hex', ax=ax, gridsize=150)
+ax.get_yaxis().set_visible(False)
+ax.get_xaxis().set_visible(False)
+#%%
+savepath='images/grande_jatte_seurat.jpg'
+fig.savefig(savepath, dpi=300, bbox_inches='tight')
+# %%
